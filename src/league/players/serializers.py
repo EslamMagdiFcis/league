5dbec001db730fd1b4b197fb950d8d8069340f30
EslamMagdiFcis <eslam.magdi.fcis@gmail.com>
2019-08-26 01:58:22 +0200

    Added partial response to player resourse

--- src/league/players/serializers.py
+++ src/league/players/serializers.py
@@ -1,9 +1,9 @@
-from rest_framework.serializers import HyperlinkedIdentityField, ModelSerializer, SerializerMethodField
+from mixins.DynamicFieldsModelSerializer import DynamicFieldsModelSerializer
 
 from .models import Player
 
 
-class PlayerSerializer(ModelSerializer):
+class PlayerSerializer(DynamicFieldsModelSerializer):
     class Meta:
         model = Player
         fields = ['id', 'first_name', 'last_name', 'gender', 'birth_date', 'position', 'shirt_number', 'team']
